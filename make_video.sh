c25ae4b9af8b3017b2dc9e2bc0152367b9b5b089
Jaerock Kwon <jaerock@umich.edu>
2023-11-13 00:16:22 -0500

    fix make_video to save mp4 near data location

--- make_video.sh
+++ make_video.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Specify a data path where images are located."  
-else 
-    rm $1.mp4
-    cat $1/*.jpg | ffmpeg -f image2pipe -framerate 15 -i - $1.mp4
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <path/to/images>"
+  exit 1
 fi
+
+input_directory=$1
+output_file="${input_directory}.mp4"
+
+ffmpeg -framerate 30 -pattern_type glob -i "${input_directory}/*.jpg" -c:v libx264 -pix_fmt yuv420p "${output_file}"
+
+echo "Video created: ${output_file}"
