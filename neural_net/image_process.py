e79351a60471c56ab2ac58cde71a7b41712689eb
Jaerock Kwon <jaerock@umich.edu>
2023-11-14 11:48:59 -0500

    remove default arg from _normalize

--- neural_net/image_process.py
+++ neural_net/image_process.py
@@ -17,24 +17,19 @@ class ImageProcess:
         return self._normalize(img)
 
 
-    def _equalize_histogram(self, img, bgr = True):
+    def _equalize_histogram(self, img):
         # img is expected as RGB
 
         img_yuv = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
         # equalize the histogram of the Y channel
         img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
-        if (bgr == True):
-            img = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-        else:
-            img = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2RGB)
+        img = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2RGB)
         return img
     
 
-    def _normalize(self, img, bgr = False):
+    def _normalize(self, img):
 
         img_norm = np.zeros_like(img)
-        if (bgr == False): # if not bgr then assume it as RGB
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
         cv2.normalize(img, img_norm, 0, 255, cv2.NORM_MINMAX)
         return img_norm
