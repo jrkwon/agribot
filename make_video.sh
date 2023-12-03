d05e2038f5d0d95ca29e7923cbc2c93e705ddf5c
bimi-alienware-aurora-r9 <jrkwon@umich.edu>
2023-12-02 23:59:08 -0500

    take care of the last /

--- make_video.sh
+++ make_video.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]; then 
   echo "Usage: $0 <path/to/images>"
   exit 1
 fi
 
 input_directory=$1
+input_directory="${input_directory%/}"
+
 output_file="${input_directory}.mp4"
 
 ffmpeg -framerate 30 -pattern_type glob -i "${input_directory}/*.jpg" -c:v libx264 -pix_fmt yuv420p "${output_file}"
