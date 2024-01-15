f41d2fd3fa2162f36e206c81bef20fb69f4807a3
bimi-alienware-aurora-r9 <jrkwon@umich.edu>
2024-01-15 14:23:34 -0500

    change default world file

--- start_simul.sh
+++ start_simul.sh
@@ -9,7 +9,7 @@ source setup.bash
 # Check if an argument is provided
 if [ $# -eq 0 ]; then
     echo "Starting Gazebo with default world..."  
-    roslaunch scout_gazebo scout_playpen.launch
+    roslaunch scout_gazebo scout_orchard_world.launch
 else
     echo "Starting Gazebo with $1..."  
     roslaunch scout_gazebo $1.launch $2 $3 $4 $5 $6
